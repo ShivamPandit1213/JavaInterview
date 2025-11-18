23fbc0237e40d48e929f0bda028192e8010e36e9
Shivam Parashar <shivamparashar1213@gmail.com>
2025-11-18 15:55:20 +0530

    Fix typo in character retrieval for string reversal

--- RevString.java
+++ RevString.java
@@ -2,8 +2,8 @@ public class RevString{
 public static void main(String[] args){
   String testName = "Hello";
   String rev = "";
-  For(int i = testName.length()-1; i >=0; i--){
-    rev+=testName.chatAt(i);
+  for(int i = testName.length()-1; i >=0; i--){
+    rev+=testName.charAt(i);
   }
   System.out.println("After Reverse String: "+rev);
 }
