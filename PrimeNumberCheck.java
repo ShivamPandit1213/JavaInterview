48883ea6a16463649fd39d391db15989d2ec0f1b
Shivam Parashar <shivamparashar1213@gmail.com>
2025-11-20 18:48:28 +0530

    Add comment for prime numbers in PrimeNumberCheck
    Added a comment listing prime numbers.

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,16 @@
+public class PrimeNumberCheck {
+  // Prime Number: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29.
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
