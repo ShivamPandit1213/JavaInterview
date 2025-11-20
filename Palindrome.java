56b6a8f66ab4711f4a94ba7e9f2729fc9dde7e1f
Shivam Parashar <shivamparashar1213@gmail.com>
2025-11-20 17:27:05 +0530

    Refactor palindrome check logic in Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+public class Palindrome{
+public static void main(String[] args){
+  
+  System.out.println("______________________________________________________________________________________________________");
+
+  String name = "madem";
+  
+  String rev = new StringBuilder(name).reverse().toString();
+  if (name.equals(rev)) 
+    System.out.println("palindrome");
+  else 
+    System.out.println("Not Palindrome");
+  System.out.println("______________________________________________________________________________________________________");
+  
+  String rev2 = "";
+  for (int i=name.length()-1; i>=0; i--){
+    rev2+=name.charAt(i);
+  } 
+  System.out.println("Original String: " + name);
+  System.out.println("Reversed String: " + rev2);
+
+      if (name.equals(rev2)) {
+            System.out.println("\nResult: Palindrome");
+      } else {
+            System.out.println("\nResult: Not Palindrome");
+      }
+  System.out.println("______________________________________________________________________________________________________");
+}
+}
